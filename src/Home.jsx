2702a437b2127d46a7e5983814068de5c03c5bce
Lorena <lorenacapraru@gmail.com>
2023-09-09 18:06:39 +0100

    homepage design done

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 const Home = () => {
   return (
     <main className="home">
-      <p className="roleH">FULL STACK DEVELOPER</p>
+      <p className="roleH">full.stack.developer</p>
       <img className="logoH" src={Logo} alt="React Logo" />
       <p className="placeH">LONDON BASED</p>
     </main>
