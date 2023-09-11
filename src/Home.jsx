6ff73fcdae52eb82600aea8778dfd6ccb9dccb8e
Lorena <lorenacapraru@gmail.com>
2023-09-11 10:08:41 +0100

    Home page completed

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,15 @@
 import Logo from "./1.png";
-import React, { useEffect, useState } from "react";
+import React from "react";
 const Home = () => {
   return (
     <main className="home">
       <p className="roleH">full.stack.developer</p>
       <img className="logoH" src={Logo} alt="React Logo" />
+      <p className="nameH">by Lorena C.</p>
+      <hr />
+
       <p className="placeH">LONDON BASED</p>
+      <button className="buttonH">Get in touch</button>
     </main>
   );
 };
