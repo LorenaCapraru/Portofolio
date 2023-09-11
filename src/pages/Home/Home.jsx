b0fa16f28cdd59b3b4c3031176602c69c2f6770a
Lorena <lorenacapraru@gmail.com>
2023-09-11 10:41:24 +0100

    Set up new Page About

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,20 @@
 import Logo from "./1.png";
 import React from "react";
+import "./Home.css";
+
 const Home = () => {
   return (
     <main className="home">
       <p className="roleH">full.stack.developer</p>
       <img className="logoH" src={Logo} alt="React Logo" />
       <p className="nameH">by Lorena C.</p>
-      <hr />
-
       <p className="placeH">LONDON BASED</p>
       <button className="buttonH">Get in touch</button>
+      <div class="arrowH">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </main>
   );
 };
