9b20d876989a16e9306a0aafcd7c54fc45cd0805
Lorena <lorenacapraru@gmail.com>
2023-09-12 14:44:41 +0100

    Skills Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Skills from "./pages/Skills/Skills";
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
+      <Skills />
     </div>
   );
 }
---
+++ src/pages/Skills/Skills.jsx
@@ -0,0 +1,110 @@
+import "./Skills.css";
+// import "src/pages/About/About.css";
+
+const Skills = () => {
+  return (
+    <main>
+      <h1 className="headerH">SKILLS</h1>
+      <h2 className="header2S">My expertise</h2>
+
+      <div class="dashboard">
+        <div className="tag">
+          <svg>
+            <circle class="bg" cx="57" cy="57" r="52" />
+            <circle class="meter-1" cx="57" cy="57" r="52" />
+          </svg>
+          <span>HTML</span>
+        </div>
+
+        <div className="tag">
+          <svg>
+            <circle class="bg" cx="57" cy="57" r="52" />
+            <circle class="meter-2" cx="57" cy="57" r="52" />
+          </svg>
+          <span>CSS</span>
+        </div>
+
+        <div className="tag">
+          <svg>
+            <circle class="bg" cx="57" cy="57" r="52" />
+            <circle class="meter-3" cx="57" cy="57" r="52" />
+          </svg>
+          <span>JavaScript</span>
+        </div>
+
+        <div className="tag">
+          <svg>
+            <circle class="bg" cx="57" cy="57" r="52" />
+            <circle class="meter-4" cx="57" cy="57" r="52" />
+          </svg>
+          <span>React.js</span>
+        </div>
+
+        <div className="tag">
+          <svg>
+            <circle class="bg" cx="57" cy="57" r="52" />
+            <circle class="meter-5" cx="57" cy="57" r="52" />
+          </svg>
+          <span>Node.js</span>
+        </div>
+
+        <div className="tag">
+          <svg>
+            <circle class="bg" cx="57" cy="57" r="52" />
+            <circle class="meter-6" cx="57" cy="57" r="52" />
+          </svg>
+          <span>Tailwind CSS</span>
+        </div>
+
+        <div className="tag">
+          <svg>
+            <circle class="bg" cx="57" cy="57" r="52" />
+            <circle class="meter-7" cx="57" cy="57" r="52" />
+          </svg>
+          <span>Jest</span>
+        </div>
+
+        <div className="tag">
+          <svg>
+            <circle class="bg" cx="57" cy="57" r="52" />
+            <circle class="meter-8" cx="57" cy="57" r="52" />
+          </svg>
+          <span>GitHub</span>
+        </div>
+
+        <div className="tag">
+          <svg>
+            <circle class="bg" cx="57" cy="57" r="52" />
+            <circle class="meter-9" cx="57" cy="57" r="52" />
+          </svg>
+          <span>Python</span>
+        </div>
+
+        <div className="tag">
+          <svg>
+            <circle class="bg" cx="57" cy="57" r="52" />
+            <circle class="meter-10" cx="57" cy="57" r="52" />
+          </svg>
+          <span>Express.js</span>
+        </div>
+
+        <div className="tag">
+          <svg>
+            <circle class="bg" cx="57" cy="57" r="52" />
+            <circle class="meter-11" cx="57" cy="57" r="52" />
+          </svg>
+          <span>Next.js</span>
+        </div>
+
+        <div className="tag">
+          <svg>
+            <circle class="bg" cx="57" cy="57" r="52" />
+            <circle class="meter-12" cx="57" cy="57" r="52" />
+          </svg>
+          <span>PostgreSQL</span>
+        </div>
+      </div>
+    </main>
+  );
+};
+export default Skills;
