06c8767218129651ca47059b6ce8fae05b6e4def
Lorena <lorenacapraru@gmail.com>
2023-09-13 15:06:46 +0100

    Style determined

--- src/pages/Portofolio/Portofolio.jsx
+++ src/pages/Portofolio/Portofolio.jsx
@@ -7,7 +7,8 @@ import project4 from "./project4.png";
 import project5 from "./project5.png";
 import project6 from "./project6.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faRocket } from "@fortawesome/free-solid-svg-icons";
 
 const Portofolio = () => {
   const [click, setClick] = useState(false);
@@ -21,8 +22,30 @@ const Portofolio = () => {
       <h1 className="headerH">PORTOFOLIO</h1>
       <h2 className="header2P">My Work</h2>
       <div className="containerP">
-        <div className="elementP">
-          <img alt="project 1" src={project1} />
+        <div
+          className={click === false ? "elementP" : "backP"}
+          onClick={handleClick}
+        >
+          {click === false ? (
+            <img alt="project 1" src={project1} />
+          ) : (
+            <div className="backCard">
+              <div className="detailsP">
+                <h2 className="nameP">GitHub Commits Tracker</h2>
+                <p>
+                  <b>Tech Stack: </b>Next.js, Prisma, Tailwind
+                </p>
+              </div>
+              <div className="socialMediaP">
+                <a href="https://github.com/momahboobian/gh-profile">
+                  <FontAwesomeIcon icon={faGithub} className="socialP" />
+                </a>
+                <a href="https://gh-profilo.vercel.app/">
+                  <FontAwesomeIcon icon={faRocket} className="socialP" />
+                </a>
+              </div>
+            </div>
+          )}
         </div>
 
         <div className="elementP">
