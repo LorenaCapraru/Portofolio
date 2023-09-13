d8e0c67b085038b1b4b62b8c54fdaa61dfcadd72
Lorena <lorenacapraru@gmail.com>
2023-09-13 10:42:52 +0100

    pages created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Skills from "./pages/Skills/Skills";
+import Portofolio from "./pages/Portofolio/Portofolio";
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
       <Skills />
+      <Portofolio />
     </div>
   );
 }
