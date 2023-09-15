3d74a5e8438fffbbc2b2b5c10a4975b2f8252eda
Lorena <lorenacapraru@gmail.com>
2023-09-15 16:22:43 +0100

    Added Skills

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
-// import Skills from "./pages/Skills/Skills";
+import Skills from "./pages/Skills/Skills";
 // import Portofolio from "./pages/Portofolio/Portofolio";
 // import Contact from "./pages/Contact/Contact";
 import ProgressBar from "./pages/ProgressBar/ProgressBar";
@@ -11,8 +11,8 @@ function App() {
       <Home />
       <About />
       <ProgressBar />
-      {/* <Skills />
-      <Portofolio />
+      <Skills />
+      {/*<Portofolio />
       <Contact />
        */}
     </div>
